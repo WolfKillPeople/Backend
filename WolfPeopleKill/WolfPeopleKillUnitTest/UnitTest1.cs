1c6332934fffe7a23170d12a33f888267b016083
blake <aa4152@gmail.com>
2020-07-11 02:29:14 +0800

    Initial Project

---
+++ WolfPeopleKill/WolfPeopleKillUnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WolfPeopleKillUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

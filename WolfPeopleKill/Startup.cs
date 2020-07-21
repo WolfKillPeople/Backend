1fd19c2096af0d565332b82a3af7472333e6f38e
blake <aa4152@gmail.com>
2020-07-21 17:21:51 +0800

    modify

--- WolfPeopleKill/Startup.cs
+++ WolfPeopleKill/Startup.cs
@@ -69,6 +69,7 @@ namespace WolfPeopleKill
             }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseSwagger();
             app.UseSwaggerUI(c =>

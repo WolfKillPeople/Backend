64a660060ccec96c135c9335889575ffa390b628
Bla3ehy <aa4152@gmail.com>
2020-07-23 17:30:04 +0800

    turn to const

--- WolfPeopleKill/Repository/DapperGameRepository.cs
+++ WolfPeopleKill/Repository/DapperGameRepository.cs
@@ -19,7 +19,7 @@ namespace WolfPeopleKill.Repository
             using (SqlConnection conn = new SqlConnection(connStr))
             {
                 conn.Open();
-                var sql = "select Occupation_Name, Occupation_GB, Pic, About from Occupation";
+                const string sql = "select Occupation_Name, Occupation_GB, Pic, About from Occupation";
                 var result = conn.Query<Occupation>(sql).ToList();
                 return result;
             }
@@ -30,7 +30,7 @@ namespace WolfPeopleKill.Repository
             using (SqlConnection conn = new SqlConnection(connStr))
             {
                 conn.Open();
-                var sql = "select * Room where @data.RoomId = RoomID";
+                const string sql = "select * Room where @data.RoomId = RoomID";
                 var result = conn.Query<Room>(sql).ToList();
                 return result;
             }
@@ -42,8 +42,8 @@ namespace WolfPeopleKill.Repository
             {
                 conn.Open();
                 var paramater = new Room { RoomId = data.RoomId, Player1 = data.Player1, Player2 = data.Player2, Player3 = data.Player3, Player4 = data.Player4, Player5 = data.Player5, Player6 = data.Player6, Player7 = data.Player7, Player8 = data.Player8, Player9 = data.Player9, Player10 = data.Player10 };
-                var sql = "update Room set RoomID = @RoomId,player1 = @Player1,player2 = @Player2,player3 = @Player3,player4 = @Player4,player5 = @Player5,player6 = @Player6,player7 = @Player7,player8 = @Player8,player9 = @Player9,player10 = @Player10";
-                conn.Query<Room>(sql, paramater);
+                const string sql = "update Room set RoomID = @RoomId,player1 = @Player1,player2 = @Player2,player3 = @Player3,player4 = @Player4,player5 = @Player5,player6 = @Player6,player7 = @Player7,player8 = @Player8,player9 = @Player9,player10 = @Player10";
+                conn.Execute(sql, paramater);
             }
         }
     }

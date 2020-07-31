e0c112cdd69a719a07b76d22be50b04d632bc431
Bla3ehy <aa4152@gmail.com>
2020-07-31 23:57:14 +0800

    .

--- WolfPeopleKill/Program.cs
+++ WolfPeopleKill/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-
+using Microsoft.Azure.AppConfiguration.AspNetCore;
 namespace WolfPeopleKill
 {
     public class Program

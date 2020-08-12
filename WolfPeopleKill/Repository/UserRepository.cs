9f3d1db1659b212f0764bb56fe422f0a752e3431
a1256963 <a1256963@gmail.com>
2020-08-12 11:31:13 +0800

    userwin point

--- WolfPeopleKill/Repository/UserRepository.cs
+++ WolfPeopleKill/Repository/UserRepository.cs
@@ -42,5 +42,42 @@ namespace WolfPeopleKill.Repository
             }
             return r;
         }
+        /// <summary>
+        /// 加減積分
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public List<UserWin> PostWin(UserWin data)
+        {
+            string connStr = "data source=werewolfkill.database.windows.net;initial catalog=Werewolfkill;persist security info=True;user id=Werewolfkill;password=Wolfpeoplekill_2020;MultipleActiveResultSets=True;";
+            List<UserWin> a = null;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                var paramater = new Models.UserWin { Email = data.Email,win=data.win };
+                var sql = "update AspNetUsers set win = @win where Email = @Email " +
+                    "select win,Email as email from AspNetUsers where Email = @Email";
+                a = conn.Query<UserWin>(sql, paramater).ToList();
+            }
+            return a;
+        }
+        /// <summary>
+        /// 讀取積分
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public List<UserWin> GetWin(UserWin data)
+        {
+            string connStr = "data source=werewolfkill.database.windows.net;initial catalog=Werewolfkill;persist security info=True;user id=Werewolfkill;password=Wolfpeoplekill_2020;MultipleActiveResultSets=True;";
+            List<UserWin> r = null;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                var paramater = new Models.UserWin { Email = data.Email };
+                var sql = "select * from AspNetUsers where Email = @Email";
+                r = conn.Query<UserWin>(sql, paramater).ToList();
+            }
+            return r;
+        }
     }
 }
